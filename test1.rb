b1e8b23315d978227d9a1913ca0946e608b3bfd4
runnerlizard <smolev_sergey@mail.ru>
2022-03-18 12:10:37 +0300

    need fix complex

--- test1.rb
+++ test1.rb
@@ -2,45 +2,51 @@
 #		launch f(arr, "-	")
 # 		in f(arr, prefix):
 # 			- check type of arr
-#			- if it is Hash or Array or Range or ComplexItem - launch f(arr[i], '	' + prefix)
-#			- else - puts(prefix + )
+#			- if it is Hash or Array or Range or ComplexItem - launch for each el f(el, tab + 1)
+#			- else - puts number of tabs
 
 class ComplexItem
 	attr_accessor :title, :items
 end
 
-def PrintSimple(str, prefix)
-	print "#{prefix}#{str}\n"
-end
 
-def InnerArray(arr, tabs)
+def InnerArray(arr, tabs, prefix)
 	if arr.is_a? Array or arr.is_a? Range
-		text = "-/Array#{tabs}:"
+		text = prefix.dup
+		text.concat("Array#{tabs}")
 		for i in 1..tabs
 			text.prepend("	")
 		end
 		puts text
 		for el in arr
-			InnerArray(el, tabs + 1)
-		end	
+			InnerArray(el, tabs + 1, text)
+		end
 	elsif arr.is_a? Hash
-		text = "-/Hash#{tabs}:"
+		text = "-/Hash#{tabs}"
 		for i in 1..tabs
 			text.prepend("	")
 		end
 		puts text
 		for el in arr
-			InnerArray(el, tabs + 1)
+			InnerArray(el, tabs + 1, text)
 		end
 	elsif arr.is_a? ComplexItem
-		puts "Complex"
-	else
-		text = "-/#{arr}"
+		text = prefix.dup
+		text.concat("/" + arr.title)
 		for i in 1..tabs
 			text.prepend("	")
 		end
 		puts text
-	end	
+		HandleItems(arr.items, tabs + 1, text)
+	else
+		text = prefix.dup
+		text.concat("/")
+		text.concat(arr.to_s)
+		for i in 1..tabs
+			text.prepend("	")
+		end
+		puts text
+	end
 end
 
 com1 = ComplexItem.new()
@@ -51,6 +57,10 @@ com2 = ComplexItem.new()
 com2.title = "Complex 2"
 com2.items = ['a', 'b', 'c']
 
+com3 = ComplexItem.new()
+com3.title = "Complex 3"
+com3.items = [com1, com2]
+
 ra = 1..9
-arr = Array[6, [1, ra, 2], "Hello", true]
-InnerArray(arr, 0)
+arr = Array[6, true, com1, "df", com2]
+InnerArray(arr, 0, "-/")
