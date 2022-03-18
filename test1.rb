4528781ea02f911aa04cf73e1e75e0db125aee53
runnerlizard <smolev_sergey@mail.ru>
2022-03-18 09:55:54 +0300

    done for arrays. todo for complex and hash

--- test1.rb
+++ test1.rb
@@ -1,7 +1,9 @@
-# in main launch f(arr)
-# in f: 
-# 		- print name of inner array
-#		- if 
+# in main:
+#		launch f(arr, "-	")
+# 		in f(arr, prefix):
+# 			- check type of arr
+#			- if it is Hash or Array or Range or ComplexItem - launch f(arr[i], '	' + prefix)
+#			- else - puts(prefix + )
 
 class ComplexItem
 	attr_accessor :title, :items
@@ -11,19 +13,35 @@ def PrintSimple(str, prefix)
 	print "#{prefix}#{str}\n"
 end
 
-def InnerArray(arr, prefix)
-	for el in arr
-		if el.is_a? ComplexItem
-			prefix.concat("/#{el}.title}")
-			puts prefix
-			prefix.prepend("	")
-			InnerArray(el.items, prefix)
-		else
-			print "#{prefix}/#{el}\n"	
+def InnerArray(arr, tabs)
+	if arr.is_a? Array or arr.is_a? Range
+		text = "-/Array#{tabs}:"
+		for i in 1..tabs
+			text.prepend("	")
 		end
-	end
+		puts text
+		for el in arr
+			InnerArray(el, tabs + 1)
+		end	
+	elsif arr.is_a? Hash
+		text = "-/Hash#{tabs}:"
+		for i in 1..tabs
+			text.prepend("	")
+		end
+		puts text
+		for el in arr
+			InnerArray(el, tabs + 1)
+		end
+	elsif arr.is_a? ComplexItem
+		puts "Complex"
+	else
+		text = "-/#{arr}"
+		for i in 1..tabs
+			text.prepend("	")
+		end
+		puts text
+	end	
 end
-
 
 com1 = ComplexItem.new()
 com1.title = "Complex 1"
@@ -33,6 +51,6 @@ com2 = ComplexItem.new()
 com2.title = "Complex 2"
 com2.items = ['a', 'b', 'c']
 
-
-arr = Array[6, com1, "Hello", true]
-InnerArray(arr, " -")
+ra = 1..9
+arr = Array[6, [1, ra, 2], "Hello", true]
+InnerArray(arr, 0)
